49d1211453cdfead7a8b35c833d2ffc8ba208bc3
mancfactor <frankmancina@gmail.com>
2022-01-05 14:39:21 -0500

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Styles -->
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+</head>
+<body class="bg-gray-100 h-screen antialiased leading-none font-sans">
+<div class="flex flex-col">
+    <div class="tekflx"></div>
+</div>
+<script src="{{ asset('/js/app.js') }}"></script>                                                          
+</body>
+</html>
