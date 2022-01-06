f807e6d06b0ba58774c6e8932b1858dd081a4334
mancfactor <frankmancina@gmail.com>
2022-01-06 17:28:49 -0500

    bug fixes and footer definition

--- resources/js/components/Tech.js
+++ resources/js/components/Tech.js
@@ -107,7 +107,7 @@ function Tech() {
                 {resource.body}
                 </p>
               </div>
-              <div class="p-6" >
+              <div className="p-6" >
                 <ul role="list" className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
                     {files.map((file) => (
                         <li key={file.source} className="relative">
